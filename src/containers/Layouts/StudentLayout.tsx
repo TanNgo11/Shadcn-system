18711d8a5cd8afd605177727b3de007e491230c5
TanNgo11 <131520745+TanNgo11@users.noreply.github.com>
2024-10-20 12:06:33 +0700

    build(shadcn): fix build faild

--- src/containers/Layouts/StudentLayout.tsx
+++ src/containers/Layouts/StudentLayout.tsx
@@ -96,7 +96,7 @@ function StudentLayout() {
                   src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
                   size: 'small',
                   title: 'Ngo Thanh Tan',
-                  render: (props, dom) => {
+                  render: (_props, dom) => {
                     return (
                       <Dropdown
                         menu={{
