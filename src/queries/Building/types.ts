1df7f67cc5f233f5d9cf1009cbd1957f94d245b0
Tan Ngo <tan_ngo@datahouse.com>
2025-05-04 18:33:31 +0700

    fix/edit-room

--- src/queries/Building/types.ts
+++ src/queries/Building/types.ts
@@ -1,61 +1,62 @@
 export interface BuildingCreationRequest {
-	code: string;
-	name: string;
-	defaultNumberOfRooms: number;
-	defaultCapacityOfRooms: number;
+  code: string;
+  name: string;
+  defaultNumberOfRooms: number;
+  defaultCapacityOfRooms: number;
 }
 
 export interface BuildingUpdateRequest {
-	code?: string;
-	name?: string;
-	defaultNumberOfRooms?: number;
-	defaultCapacityOfRooms?: number;
+  code?: string;
+  name?: string;
+  defaultNumberOfRooms?: number;
+  defaultCapacityOfRooms?: number;
 }
 
 export interface BuildingResponse {
-	id: number;
-	code: string;
-	name: string;
-	rooms: RoomResponse[];
+  id: number;
+  code: string;
+  name: string;
+  rooms: RoomResponse[];
 }
 
 export interface RoomCreationRequest {
-	code: string;
-	name: string;
-	capacity: number;
-	roomType: RoomType;
+  code: string;
+  name: string;
+  capacity: number;
+  roomType: RoomType;
 }
 
 export interface RoomUpdateRequest {
-	code?: string;
-	name?: string;
-	capacity?: number;
-	roomType?: RoomType;
+  code?: string;
+  name?: string;
+  capacity?: number;
+  roomType?: RoomType;
 }
 
 export interface RoomResponse {
-	code: string;
-	name: string;
-	capacity: number;
-	roomType: RoomType;
-	status: RoomStatus;
-	buildingCode: string;
+  id?: number;
+  code: string;
+  name: string;
+  capacity: number;
+  roomType: RoomType;
+  status: RoomStatus;
+  buildingCode: string;
 }
 
 export enum RoomType {
-	CLASSROOM = 'CLASSROOM',
-	LABORATORY = 'LABORATORY',
-	OFFICE = 'OFFICE',
-	AUDITORIUM = 'AUDITORIUM',
-	CONFERENCE_ROOM = 'CONFERENCE_ROOM',
-	STUDY_ROOM = 'STUDY_ROOM',
-	OTHER = 'OTHER',
+  CLASSROOM = 'CLASSROOM',
+  LABORATORY = 'LABORATORY',
+  OFFICE = 'OFFICE',
+  AUDITORIUM = 'AUDITORIUM',
+  CONFERENCE_ROOM = 'CONFERENCE_ROOM',
+  STUDY_ROOM = 'STUDY_ROOM',
+  OTHER = 'OTHER',
 }
 
 export enum RoomStatus {
-	AVAILABLE = 'AVAILABLE',
-	OCCUPIED = 'OCCUPIED',
-	UNDER_MAINTENANCE = 'UNDER_MAINTENANCE',
-	RESERVED = 'RESERVED',
-	UNAVAILABLE = 'UNAVAILABLE',
+  AVAILABLE = 'AVAILABLE',
+  OCCUPIED = 'OCCUPIED',
+  UNDER_MAINTENANCE = 'UNDER_MAINTENANCE',
+  RESERVED = 'RESERVED',
+  UNAVAILABLE = 'UNAVAILABLE',
 }
